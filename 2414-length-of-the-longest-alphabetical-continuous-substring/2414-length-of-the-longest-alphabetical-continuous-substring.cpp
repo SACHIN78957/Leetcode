25989cf3220b067d710ca830f75c856c5d5172ad
SACHIN78957 <sachinlakheda@gmail.com>
2026-09-16 01:28:04 +0530

    Time: 5 ms (37.46%), Space: 18.1 MB (21.72%) - LeetHub

---
+++ 2414-length-of-the-longest-alphabetical-continuous-substring/2414-length-of-the-longest-alphabetical-continuous-substring.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int longestContinuousSubstring(string s) {
+        int longest=1;
+        int length=1;
+        for(int i=1;i<s.length();i++)
+        {
+            if(s[i]==s[i-1]+1)
+            {
+                length++;
+            }
+            else{
+                length=1;
+            }
+            longest=max(longest,length);
+        }
+        return longest;
+    }
+};
